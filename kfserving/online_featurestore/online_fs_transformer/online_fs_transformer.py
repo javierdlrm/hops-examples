53ba42068040d73faceff2191d3a9a6f479db091
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-09-30 11:14:56 +0200

    Fix fs config

--- kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
+++ kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
@@ -12,18 +12,16 @@ PROJECT = "FS_PROJECT"
 API_KEY = "FS_API_KEY"
 SECRETS_LOCAL = "local"
 
+
 def get_feature_vectors(fs, instances):
 
     # Get feature groups
     # teams_fg = fs.get_feature_group('teams_features', 1)
-    # players_fg = fs.get_feature_group('players_features', 1)
-    
+    # players_fg = fs.get_feature_group('players_features', 1)    
     # Select features
     # features = teams_fg.select(['team_position', 'team_budget']).join(
     #     players_fg.select(['average_player_worth', 'average_player_age', 'average_player_rating']))
-
     # features_df = fs.sql("SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget, p_fg.average_player_worth, p_fg.average_player_age, p_fg.average_player_rating FROM `teams_features_1` t_fg, `players_features_1` p_fg WHERE t_fg.team_id==1 AND p_fg.team_id==1")
-    
     # df = fs.sql("SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age FROM `teams_features_1` t_fg LEFT JOIN `players_features_1` p_fg ON t_fg.team_id = p_fg.team_id WHERE t_fg.team_id = 1")
     # instance = df.to_dict(orient="records")[0]
 
@@ -44,7 +42,7 @@ def get_feature_vectors(fs, instances):
     for fv in features_df.to_numpy():
         logging.info(fv)
 
-    return [[6.8,  2.8,  4.8,  1.4], [6.0,  3.4,  4.5,  1.6]] # fake input for flower iris model
+    return [[6.8, 2.8, 4.8, 1.4], [6.0, 3.4, 4.5, 1.6]]  # fake input for flower iris model
 
 
 def get_feature_store_config():
@@ -75,11 +73,11 @@ class OnlineFSTransformer(kfserving.KFModel):
         super().__init__(name)
         self.predictor_host = predictor_host
         self.fs_config = get_feature_store_config()
-        self.fs = get_feature_store_connector(fs_config)
+        self.fs = get_feature_store_connector(self.fs_config)
 
     def preprocess(self, inputs: Dict) -> Dict:
         logging.info("Getting feature vectors for Teams [" + [i.team_id for i in inputs['instances']].join(', ') + "] ...")
         return {'instances': get_feature_vectors(self.fs, inputs['instances']) }
-        
+
     def postprocess(self, inputs: List) -> List:
         return inputs
