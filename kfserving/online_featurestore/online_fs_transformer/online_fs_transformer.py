2b22acac538d7170689a076398096f51749a7ec4
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-09-30 15:47:02 +0200

    Add three latency tests

--- kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
+++ kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
@@ -1,4 +1,6 @@
 import logging
+import time
+from multiprocessing import Process, Queue
 from typing import List, Dict
 import kfserving
 import hsfs
@@ -9,32 +11,74 @@ logging.basicConfig(level=kfserving.constants.KFSERVING_LOGLEVEL)
 
 def get_feature_vectors(fs, instances):
 
-    # Get feature groups
-    # teams_fg = fs.get_feature_group('teams_features', 1)
-    # players_fg = fs.get_feature_group('players_features', 1)    
-    # Select features
-    # features = teams_fg.select(['team_position', 'team_budget']).join(
-    #     players_fg.select(['average_player_worth', 'average_player_age', 'average_player_rating']))
-    # features_df = fs.sql("SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget, p_fg.average_player_worth, p_fg.average_player_age, p_fg.average_player_rating FROM `teams_features_1` t_fg, `players_features_1` p_fg WHERE t_fg.team_id==1 AND p_fg.team_id==1")
-    # df = fs.sql("SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age FROM `teams_features_1` t_fg LEFT JOIN `players_features_1` p_fg ON t_fg.team_id = p_fg.team_id WHERE t_fg.team_id = 1")
-    # instance = df.to_dict(orient="records")[0]
-
-    logging.info("-------------------------------")
-
-    logging.info("Getting team ids")
     team_ids = [str(instance['team_id']) for instance in instances]
+    logging.info("Getting feature vectors for teams" + ', '.join(team_ids))
 
-    logging.info("Getting features")
+    # One sql query
+
+    logging.info("#1 -- Using one sql query (join, where)--")
+
+    start = time.perf_counter()
     features_df = fs.sql("""SELECT t_fg.team_position, t_fg.team_budget, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age
                             FROM `teams_features_1` t_fg
                             LEFT JOIN `players_features_1` p_fg
                             ON t_fg.team_id = p_fg.team_id
                             WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")")
+    logging.info(f"-- {time.perf_counter() - start}")
 
-    logging.info("Map to feature vectors")
-    logging.info("Feature vectors:")
-    for fv in features_df.to_numpy():
-        logging.info(fv)
+    # Multiple sql queries sequentially
+
+    logging.info("#2 -- Using multiple sql query sequentially--")
+
+    start = time.perf_counter()
+
+    teams_df = fs.sql("""SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget
+                         FROM `teams_features_1` t_fg
+                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")")
+    players_df = fs.sql("""SELECT p_fg.team_id, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age
+                         FROM `players_features_1` p_fg
+                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")")
+    features_df = teams_df.join(players_df.set_index('p_fg.team_id'), on='t_fg.team_id')
+    features_df = features_df.drop(columns=['t_fg.team_id'])
+
+    logging.info(f"-- {time.perf_counter() - start}")
+
+    # Multiple sql queries parallelly
+
+    logging.info("#3 -- Using multiple sql query parallelly--")
+
+    start = time.perf_counter()
+
+    def get_features(fs, query, queue):
+        queue.put(fs.sql(query))
+
+    q = Queue()
+    procs = []
+    dfs = []
+
+    procs.append(Process(target=get_features, args=(fs, """SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget
+                         FROM `teams_features_1` t_fg
+                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")", q)))
+    procs.append(Process(target=get_features, args=(fs, """SELECT p_fg.team_id, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age
+                         FROM `players_features_1` p_fg
+                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")", q)))
+
+    for p in procs: p.start()
+
+    for p in procs:
+        df = q.get()
+        df.columns = [c.split('.')[1] for c in df.columns]
+        dfs.append(df)
+
+    for p in procs: p.join()
+
+    features_df = dfs[0].join(dfs[1].set_index('team_id'), on='team_id')
+    features_df = features_df.drop(columns=['team_id'])
+
+    logging.info(f"-- {time.perf_counter() - start}")
+
+    # for fv in features_df.to_numpy():
+    #     logging.info(fv)
 
     # fake input for flower iris model
     return [[6.8, 2.8, 4.8, 1.4], [6.0, 3.4, 4.5, 1.6]]
@@ -62,8 +106,8 @@ class OnlineFSTransformer(kfserving.KFModel):
         self.fs = get_feature_store_connector(self.fs_config)
 
     def preprocess(self, inputs: Dict) -> Dict:
-        logging.info("Getting feature vectors for Teams [" + ','.join([str(i['team_id']) for i in inputs['instances']]) + "] ...")
-        return {'instances': get_feature_vectors(self.fs, inputs['instances'])}
+        feature_vectors = get_feature_vectors(self.fs, inputs['instances'])
+        return {'instances': feature_vectors}
 
     def postprocess(self, inputs: List) -> List:
         return inputs
