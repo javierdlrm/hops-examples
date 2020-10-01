321e6c4fbca04738b0f8fa348d8bf1be8a8309cf
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2020-10-01 11:24:20 +0200

    Set online storage

--- kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
+++ kfserving/online_featurestore/online_fs_transformer/online_fs_transformer.py
@@ -23,7 +23,9 @@ def get_feature_vectors(fs, instances):
                             FROM `teams_features_1` t_fg
                             LEFT JOIN `players_features_1` p_fg
                             ON t_fg.team_id = p_fg.team_id
-                            WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")")
+                            WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")",
+                         dataframe_type="pandas",
+                         storage="online")
     logging.info(f"-- {time.perf_counter() - start}")
 
     # Multiple sql queries sequentially
@@ -34,10 +36,14 @@ def get_feature_vectors(fs, instances):
 
     teams_df = fs.sql("""SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget
                          FROM `teams_features_1` t_fg
-                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")")
+                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")",
+                      dataframe_type="pandas",
+                      storage="online")
     players_df = fs.sql("""SELECT p_fg.team_id, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age
                          FROM `players_features_1` p_fg
-                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")")
+                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")",
+                        dataframe_type="pandas",
+                        storage="online")
     features_df = teams_df.join(players_df.set_index('p_fg.team_id'), on='t_fg.team_id')
     features_df = features_df.drop(columns=['t_fg.team_id'])
 
@@ -50,23 +56,23 @@ def get_feature_vectors(fs, instances):
     start = time.perf_counter()
 
     def get_features(fs, query, queue):
-        queue.put(fs.sql(query))
+        queue.put(fs.sql(query, dataframe_type="pandas", storage="online"))
 
-    q = Queue()
+    queue = Queue()
     procs = []
     dfs = []
 
     procs.append(Process(target=get_features, args=(fs, """SELECT t_fg.team_id, t_fg.team_position, t_fg.team_budget
                          FROM `teams_features_1` t_fg
-                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")", q)))
+                         WHERE t_fg.team_id IN (""" + ','.join(team_ids) + ")", queue)))
     procs.append(Process(target=get_features, args=(fs, """SELECT p_fg.team_id, p_fg.average_player_rating, p_fg.average_player_worth, p_fg.average_player_age
                          FROM `players_features_1` p_fg
-                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")", q)))
+                         WHERE p_fg.team_id IN (""" + ','.join(team_ids) + ")", queue)))
 
     for p in procs: p.start()
 
     for p in procs:
-        df = q.get()
+        df = queue.get()
         df.columns = [c.split('.')[1] for c in df.columns]
         dfs.append(df)
 
